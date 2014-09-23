7ccb7b70ddecb01c48b53f8c1498b9440ef38993
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2014-09-23 15:37:01 +0300

    4 basic math operation tests are passed

--- lib/simple_calcu.rb
+++ lib/simple_calcu.rb
@@ -4,4 +4,13 @@ module SimpleCalcu
   def self.add(number1, number2)
     number1 + number2
   end
+  def self.sub(number1, number2)
+    number1 - number2
+  end
+  def self.mul(number1, number2)
+    number1 * number2
+  end
+  def self.div(number1, number2)
+    number1 / number2
+  end
 end
--- test/calculator_test.rb
+++ test/calculator_test.rb
@@ -5,4 +5,13 @@ class CalculatorTest < Minitest::Test
   def test_addition
     assert_equal 5, SimpleCalcu::add(3, 2)
   end
+  def test_subtraction
+    assert_equal 7, SimpleCalcu::sub(8, 1)
+  end
+  def test_multiplication
+    assert_equal 25, SimpleCalcu::mul(5, 5)
+  end
+  def test_division
+    assert_equal 5, SimpleCalcu::div(10, 2)
+  end
 end
